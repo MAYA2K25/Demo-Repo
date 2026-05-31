1628c3c5acd2342b6938fb73b3d625ad10e1cf4e
MAYA2k25 <mahi2k25@gmail.com>
2026-05-31 07:24:28 -0500

    add input chnages to tipcalcy

--- tip_calcy.py
+++ tip_calcy.py
@@ -1,14 +1,19 @@
 import logging
 
 logging.basicConfig(level=logging.INFO)
+
 def tip_calculator(Bamount, rate=0.01):
-    logging.info(f"Initializing the tip calulator")
+    logging.info("Initializing the tip calculator")
     logging.info(f"Total Bill Amount is: {Bamount}")
-    tip=Bamount*rate
-    finalamount=Bamount+tip
+    tip = Bamount * rate
+    finalamount = Bamount + tip
     logging.info(f"Final amount for the order is {finalamount}")
-    
-Bamount=float(input(f"Emter the Bill Amount:"))
-print(tip_calculator(Bamount))
+    return tip, finalamount
 
+def get_input():
+    Bamount = float(input("Enter the total bill amount: "))
+    return Bamount
 
+Bamount = get_input()
+tip, finalamount = tip_calculator(Bamount)
+print(f"Tip: {tip}, Final Amount: {finalamount}")
